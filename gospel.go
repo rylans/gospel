655237e4d3e9cda1f784be69d9c09311b1bd34c6
Rylan <rylans@gmail.com>
2018-01-26 11:17:37 -0800

    Load words from usr/share/dict/words

--- gospel.go
+++ gospel.go
@@ -1,6 +1,49 @@
 package gospel
 
-import "fmt"
+import (
+  "fmt"
+  "io/ioutil"
+  "strings"
+  "strconv"
+)
+
+type Corrector struct {
+  descriptor string
+  dictionaryWords map[string]int
+}
+
+func (corr Corrector) String() string {
+  return corr.descriptor
+}
+
+func (corr *Corrector) Contains(str string) bool {
+  _, exists := corr.dictionaryWords[str]
+  return exists
+}
+
+
+// Corrector builders
+func ForEnglish() Corrector {
+  dictWords := make(map[string]int)
+
+  for _, w := range readWords() {
+    dictWords[strings.ToLower(w)] = 1
+  }
+
+  numWords := strconv.Itoa(len(dictWords))
+  desc := "English spelling corrector (words loaded: " + numWords + ")"
+  return Corrector{descriptor: desc, dictionaryWords: dictWords}
+
+}
+
+
+func readWords() []string {
+  dat, err := ioutil.ReadFile("/usr/share/dict/words")
+  if err != nil {
+    panic(err)
+  }
+  return strings.Split(string(dat), "\n")
+}
 
 func IsCorrect(str string) (bool, error) {
   if "correct" == str {
@@ -11,7 +54,10 @@ func IsCorrect(str string) (bool, error) {
 }
 
 func main(){
-  fmt.Println("hello")
 
+  x := ForEnglish()
+  fmt.Println(x)
+  fmt.Println(x.Contains("scolytus"))
+  fmt.Println(x.Contains("skolytus"))
 }
 
