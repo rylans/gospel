3a02666f670f070f06a12f63d93a420cd3794a64
Rylan <rylans@gmail.com>
2018-01-26 14:11:43 -0800

    Implement correction for one letter deletion

--- gospel.go
+++ gospel.go
@@ -26,21 +26,37 @@ func (corr *Corrector) Correct(str string) (string, error) {
   if corr.Contains(str) { 
     return str, nil
   } 
+
+  for _, word := range edits(str) {
+    if corr.Contains(word) {
+      return word, nil
+    }
+  }
+
   return "", errors.New("Unable to correct '" + str + "'")
 }
 
-
 // Corrector builders
 func ForEnglish() Corrector {
-  dictWords := make(map[string]int)
-
-  for _, w := range readWords() {
-    dictWords[strings.ToLower(w)] = 1
-  }
-
+  dictWords := makeDictOfWords(readWords())
   numWords := strconv.Itoa(len(dictWords))
   desc := "English spelling corrector (words loaded: " + numWords + ")"
   return Corrector{descriptor: desc, dictionaryWords: dictWords}
+}
+
+func OfWords(words []string) Corrector {
+  dictWords := makeDictOfWords(words)
+  numWords := strconv.Itoa(len(dictWords))
+  desc := "Spelling corrector (words loaded: " + numWords + ")"
+  return Corrector{descriptor: desc, dictionaryWords: dictWords}
+}
+
+func makeDictOfWords(words []string) map[string]int {
+  dictWords := make(map[string]int)
+  for _, w := range words {
+    dictWords[strings.ToLower(w)] = 1
+  }
+  return dictWords
 }
 
 
@@ -60,11 +76,24 @@ func IsCorrect(str string) (bool, error) {
   }
 }
 
-func main(){
+func edits(word string) []string {
+  splits := []string{}
 
+  // deletions
+  for i, _ := range word {
+    splits =  append(splits, word[:i] + word[i+1:])
+  }
+
+  return splits
+}
+
+
+func main(){
   x := ForEnglish()
   fmt.Println(x)
   fmt.Println(x.Contains("scolytus"))
   fmt.Println(x.Contains("skolytus"))
+
+  fmt.Println(edits("knight"))
 }
 
--- gospel_test.go
+++ gospel_test.go
@@ -5,6 +5,16 @@ import (
   "github.com/stretchr/testify/assert"
 )
 
+func TestWordsCorrectorContainsWord(t *testing.T){
+  c := OfWords([]string{"hello", "there"})
+  assert.Equal(t, true, c.Contains("There"), "should contain word 'there'")
+}
+
+func TestWordsCorrectorNotContainsWord(t *testing.T){
+  c := OfWords([]string{"hello", "there"})
+  assert.Equal(t, false, c.Contains("seven"), "should not contain word 'seven'")
+}
+
 func TestEnglishCorrectorNotContainsWord(t *testing.T){
   c := ForEnglish()
   assert.Equal(t, false, c.Contains("FOOBAR"), "should not contain non-existant word")
@@ -29,3 +39,25 @@ func TestEnglishCorrectorFailsToCorrectWord(t *testing.T){
   assert.Equal(t, "", corrected, "should have returned empty string")
   assert.NotNil(t, err, "should have returned an error")
 }
+
+func TestWordsCorrectorCorrectsDeletionInitial(t *testing.T){
+  c := OfWords([]string{"knight"})
+
+  corrected, _ := c.Correct("zknight")
+  assert.Equal(t, "knight", corrected, "should have corrected to word 'knight'")
+}
+
+func TestWordsCorrectorCorrectsDeletionFinal(t *testing.T){
+  c := OfWords([]string{"knight"})
+
+  corrected, _ := c.Correct("knightx")
+  assert.Equal(t, "knight", corrected, "should have corrected to word 'knight'")
+}
+
+func TestWordsCorrectorCorrectsDeletionMedial(t *testing.T){
+  c := OfWords([]string{"knight"})
+
+  corrected, _ := c.Correct("knieght")
+  assert.Equal(t, "knight", corrected, "should have corrected to word 'knight'")
+}
+
