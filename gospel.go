4374f179563beb9501db3f3688d3698f88c02abf
Rylan <rylans@gmail.com>
2018-01-26 10:14:53 -0800

    introduce IsCorrect function

--- gospel.go
+++ gospel.go
@@ -2,6 +2,14 @@ package gospel
 
 import "fmt"
 
+func IsCorrect(str string) (bool, error) {
+  if "correct" == str {
+    return true, nil
+  } else {
+    return false, nil
+  }
+}
+
 func main(){
   fmt.Println("hello")
 
